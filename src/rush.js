264cbb53cf651e137196faf49b4b84537ef519cf
Natalie Ramirez <61169728+natalieramirez22@users.noreply.github.com>
2024-06-23 17:13:37 -0400

    start rush page

--- src/rush.js
+++ src/rush.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import CalendarIcon from './CalendarIcon';
+import PinIcon from './PinIcon';
 
 function Rush() {
   return (
@@ -28,7 +30,72 @@ function Rush() {
           </a>
         </div>
       </div>
+      
+      <div className="px-32">
+      {/* Page content */}
+
+        <div className="relative py-32 z-10">
+          {/* Header */}
+          <div className="flex flex-col text-center">
+            <h1 className="text-5xl font-black">Learn about Rushing!</h1>
+            <p className=" text-xl mt-4 mb-32 font-medium" style={{ color: 'grey' }}>
+              Applications open on xyz, learn more about KTP. Here's our rush schedule.
+            </p>
+          </div>
+        </div>
+
+
+        {/* Timeline */}
+
+        {/* Open House */}
+        <div className="relative">
+          <div className="border-l-4 border-blue-700 absolute h-full left-4 top-0"></div>
+          <div className="mb-10 flex items-center">
+            <div>
+              <div className="w-4 h-4 rounded-full bg-blue-700 text-white flex items-center justify-center relative left-0.5 -ml-5" />
+            </div>
+            <div className="ml-12">
+              <h2 className="text-xl font-bold mb-2">Open House</h2>
+              <p className="text-gray-600 flex items-center mb-2">
+                <CalendarIcon className="mr-4"/> January 22nd • <PinIcon className="ml-2 mr-2"/> Blau Auditorium
+              </p>
+              <p className="text-gray-600 flex items-center">Join us for one of our Open Houses! First, we'll give a presentation about what it means to be a brother in KTP.</p>
+            </div>
+          </div>
+
+          {/* Application Deadline */}
+          <div className="mb-10 flex items-center">
+            <div className="w-4 h-4 rounded-full bg-blue-700 text-white flex items-center justify-center relative left-0.5 -ml-5">
+            </div>
+            <div className="ml-12">
+              <h2 className="text-xl font-bold mb-2">Application Deadline</h2>
+              <p className="text-gray-600 flex items-center mb-2">
+                <CalendarIcon className="mr-4"/> January 22nd • <PinIcon className="ml-2 mr-2"/> Blau Auditorium
+              </p>
+              <button className="mt-2 px-4 py-2 bg-purple-600 text-white rounded-lg">Submit on Forms</button>
+            </div>
+          </div>
+
+          {/* Meet & Greet */}
+          <div className="relative">
+          <div className="border-l-4 border-blue-700 absolute h-full left-4 top-0"></div>
+          <div className="mb-10 flex items-center">
+            <div>
+              <div className="w-4 h-4 rounded-full bg-blue-700 text-white flex items-center justify-center relative left-0.5 -ml-5" />
+            </div>
+            <div className="ml-12">
+              <h2 className="text-xl font-bold mb-2">Meet & Greet</h2>
+              <p className="text-gray-600 flex items-center mb-2">
+                <CalendarIcon className="mr-4"/> January 22nd • <PinIcon className="ml-2 mr-2"/> Blau Auditorium
+              </p>
+              <p className="text-gray-600 flex items-center">In this event you can meet the brothers & xyz.</p>
+            </div>
+          </div>
+          </div>
+        </div>
+      </div>
     </div>
+
   );
 }
 
