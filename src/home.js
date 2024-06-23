246cdeb6e1a51b45dbfb9f67035f06f5af83cba4
Daniel Yang <ydaniel@umich.edu>
2024-06-23 17:28:27 -0400

    highkey i forgot lmao but probably blobs

--- src/home.js
+++ src/home.js
@@ -4,9 +4,14 @@ import { Link } from 'react-router-dom';
 function Home() {
   return (
     <div>
-      <div className="blob-c">
-        <div className="shape-blob one"></div>
-        <div className="shape-blob two"></div>
+      <div class="blob-c">
+        <div class="shape-blob"></div>
+        <div class="shape-blob one"></div>
+        <div class="shape-blob two"></div>
+        <div class="shape-blob three"></div>
+        <div class="shape-blob four"></div>
+        <div class="shape-blob five"></div>
+        <div class="shape-blob six"></div>
       </div>
 
       {/* Top bar */}
