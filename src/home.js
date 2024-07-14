82db76988ef5cd94a060cfa1e5a3469ea746ad6d
Daniel Yang <ydaniel@umich.edu>
2024-07-14 17:36:31 -0400

    new homepage, start of footer

--- src/home.js
+++ src/home.js
@@ -1,74 +1,85 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './App.css';
 
 function Home() {
   return (
-    <div>
-      <div class="blob-c">
+    <div className='homepage-grid'>
+      {/* <div class="blob-c">
         <div class="shape-blob six"></div>
         <div class="shape-blob one"></div>
         <div class="shape-blob four"></div>
         <div class="shape-blob five"></div>
         <div class="shape-blob three"></div>
         <div class="shape-blob two"></div>
+      </div> */}
+      <div className='glowing-circle z-2'></div>
+      <div className='glowing-circle-2 z-1'></div>
+
+      {/* Main content */}
+      <div className='homepage-content'>
+        {/* Top bar */}
+        <div className="relative flex items-center justify-between w-full p-4 z-10">
+          {/* Logo */}
+          <div className='flex justify-start'>
+            <a href='/'>
+              <img src={`${process.env.PUBLIC_URL}/ktp_logo.png`} alt="Logo" className="w-32 h-auto" />
+            </a>
+          </div>
+
+          {/* Nav */}
+          <div className='flex justify-center space-x-20'>
+            <Link className="hover:text-blue-700 font-medium" to="/about">About Us</Link>
+            <Link className="hover:text-blue-700 font-medium" to="/rush">Rush</Link>
+            <Link className="hover:text-blue-700 font-medium" to="/members">Members</Link>
+            <Link className="hover:text-blue-700 font-medium" to="/nationals">Nationals</Link>
+          </div>
+
+          {/* Life App */}
+          <div className='flex justify-end'>
+            <a href='/lifeapp'>
+              <img src={`${process.env.PUBLIC_URL}/life_app.png`} alt='Life App' className="w-40 h-auto" />
+            </a>
+          </div>
+        </div>
+
+        {/* Middle */}
+        <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex flex-col items-center justify-center'>
+          <div className="text-5xl font-bold z-10 text-center" style={{ fontFamily: 'Inter, sans-serif', fontWeight: '900', letterSpacing: '-0.02em' }}>
+            <div className='mb-2'>We’re the University of</div>
+            <div className='mb-2'>Michigan’s premier technology</div>
+            <div>fraternity</div>
+          </div>
+
+          <div className='text-[#707070] text-[16px] text-center mt-12'>
+            <div>Founded in 2012 as the first KTP nationally, we're dedicated to uniting</div>
+            <div>students across the nation for the love of technology</div>
+          </div>
+
+          <div className='contact-us text-blue-700 font-semibold text-lg mt-12 flex flex-row items-center justify-center' style={{ fontFamily: 'Inter, sans-serif' }}>
+            {/* <img src={`${process.env.PUBLIC_URL}/umich.png`} alt="Logo" className="w-8 h-auto" /> */}
+            <Link to="/rush">Rush</Link>
+          </div>
+        </div>
       </div>
 
-      {/* Top bar */}
-      <div className="relative flex items-center justify-between w-full p-4 z-10">
-        {/* Logo */}
-        <div className='flex justify-start'>
-          <a href='/'>
-            <img src={`${process.env.PUBLIC_URL}/ktp_logo.png`} alt="Logo" className="w-32 h-auto" />
-          </a>
-        </div>
-
-        {/* Nav */}
-        <div className='flex justify-center space-x-20'>
-          <Link className="hover:text-blue-700 font-medium" to="/about">About Us</Link>
-          <Link className="hover:text-blue-700 font-medium" to="/rush">Rush</Link>
-          <Link className="hover:text-blue-700 font-medium" to="/members">Members</Link>
-          <Link className="hover:text-blue-700 font-medium" to="/nationals">Nationals</Link>
-        </div>
-
-        {/* Life App */}
-        <div className='flex justify-end'>
-          <a href='/lifeapp'>
-            <img src={`${process.env.PUBLIC_URL}/life_app.png`} alt='Life App' className="w-40 h-auto" />
-          </a>
-        </div>
+      {/* Footer */}
+      <div className="footer flex items-center justify-between">
+        idk what we want here yet
       </div>
 
-      {/* Middle */}
-      <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex flex-col items-center justify-center'>
-        <div className="text-5xl font-bold z-10 text-center" style={{ fontFamily: 'Inter, sans-serif', fontWeight: '900', letterSpacing: '-0.02em' }}>
-          <div className='mb-2'>We’re the University of</div>
-          <div className='mb-2'>Michigan’s premier technology</div>
-          <div>fraternity</div>
-        </div>
-
-        <div className='text-[#707070] text-[16px] text-center mt-12'>
-          <div>Founded in 2012 as the first KTP nationally, we're dedicated to uniting</div>
-          <div>students across the nation for the love of technology</div>
-        </div>
-
-        <div className='contact-us text-blue-700 font-semibold text-lg mt-12 flex flex-row items-center justify-center' style={{ fontFamily: 'Inter, sans-serif' }}>
-          {/* <img src={`${process.env.PUBLIC_URL}/umich.png`} alt="Logo" className="w-8 h-auto" /> */}
-          <div>Rush</div>
-        </div>
-      </div>
-
-      {/* Images
+      {/* Images left side */}
       <img
-        src={`${process.env.PUBLIC_URL}/cross.JPEG`}
+        src={`${process.env.PUBLIC_URL}/yummers.jpeg`}
         alt="image"
         style={{
           position: 'absolute',
           top: '15%',
-          left: '6%',
+          left: '8%',
           width: '14vw',
           height: 'auto',
           transform: 'rotate(15deg)',
-          borderRadius: '10px'
+          borderRadius: '25px'
         }}
       />
 
@@ -78,25 +89,26 @@ function Home() {
         style={{
           position: 'absolute',
           top: '55%',
-          left: '6%',
+          left: '10%',
           width: '14vw',
           height: 'auto',
           transform: 'rotate(-19deg)',
-          borderRadius: '10px'
+          borderRadius: '25px'
         }}
       />
 
+      {/* Images right side */}
       <img
         src={`${process.env.PUBLIC_URL}/paddle.jpg`}
         alt="image"
         style={{
           position: 'absolute',
-          top: '4%',
-          left: '93%',
-          width: '12vw',
+          top: '0%',
+          left: '90%',
+          width: '13vw',
           height: 'auto',
-          transform: 'rotate(20deg)',
-          borderRadius: '10px'
+          transform: 'rotate(13deg)',
+          borderRadius: '25px'
         }}
       />
 
@@ -105,12 +117,12 @@ function Home() {
         alt="image"
         style={{
           position: 'absolute',
-          top: '37%',
-          left: '80%',
+          top: '45%',
+          left: '81%',
           width: '15vw',
           height: 'auto',
-          transform: 'rotate(20deg)',
-          borderRadius: '10px'
+          transform: 'rotate(13deg)',
+          borderRadius: '25px'
         }}
       />
       <img
@@ -118,14 +130,14 @@ function Home() {
         alt="image"
         style={{
           position: 'absolute',
-          top: '55%',
-          left: '96%',
+          top: '70%',
+          left: '95%',
           width: '15vw',
           height: 'auto',
-          transform: 'rotate(20deg)',
-          borderRadius: '10px'
+          transform: 'rotate(13deg)',
+          borderRadius: '25px'
         }}
-      /> */}
+      />
     </div>
   );
 }
